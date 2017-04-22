dab6c3bdeddef1fb63aef1e19f1453007d42e33a
wan <ansatgol@gmail.com>
2017-04-22 22:46:34 +0900

    fixed Ready.md

--- src/main/java/com/wan/basis/controller/SecurityController.java
+++ src/main/java/com/wan/basis/controller/SecurityController.java
@@ -1,14 +1,11 @@
 package com.wan.basis.controller;
 
 import java.text.DateFormat;
-
-
 import java.util.Date;
 import java.util.Locale;
 import java.util.UUID;
 
 import javax.annotation.Resource;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,7 +19,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -45,9 +41,6 @@ public class SecurityController {
 	
 	@Autowired
 	BCryptPasswordEncoder passwordEncoder;
-	
-//	@Autowired
-//	UserDao dao;
 	
 	private static final Logger logger = LoggerFactory.getLogger(SecurityController.class);
 	@Resource
@@ -79,26 +72,6 @@ public class SecurityController {
 		return "home";
 	}
 	
-//	@RequestMapping(value = "/admin", method = RequestMethod.GET) //유저만 들어갈수있
-//	public String moveUser(Locale locale, Model model) {
-//		logger.info("Admin");
-//		
-//		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//		String username = auth.getName();
-//		
-//		
-//		model.addAttribute("username",username);
-//		
-//		Date date = new Date();
-//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-//		
-//		String formattedDate = dateFormat.format(date);
-//		
-//		model.addAttribute("serverTime", formattedDate );
-//		
-//		return "security/admin";
-//	}
-	
 	@RequestMapping(value = "/user", method = RequestMethod.GET) //유저만 들어갈수있
 	public String moveAdmin(Locale locale, Model model) {
 		logger.info("User");
@@ -119,21 +92,6 @@ public class SecurityController {
 		return "security/user";
 	}
 	
-//	
-//	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
-//	public String welcome(Locale locale, Model model) {
-//		logger.info("Welcome home! The client locale is {}.", locale);
-//		
-//		Date date = new Date();
-//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-//		
-//		String formattedDate = dateFormat.format(date);
-//		
-//		model.addAttribute("serverTime", formattedDate );
-//		
-//		return "security/welcome";
-//	}
-	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginForm(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -147,18 +105,7 @@ public class SecurityController {
 		
 		return "security/loginForm";
 	}
-	
-//	@RequestMapping(value = "/age", method = RequestMethod.GET) //유저 정보 불러오는 부분 
-//	public String test(Locale locale, Model model) {
-//		logger.info("test()");
-//		
-//		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//		User user = (User)authentication.getPrincipal();
-//		System.out.println(user.getAge());
-//
-//		
-//		return "security/user";
-//	}
+
 	
 	@ResponseBody
 	@RequestMapping(value = "/create", method = RequestMethod.POST) //회원가입부
@@ -176,19 +123,5 @@ public class SecurityController {
 		
 		return "security/user";
 	}
-	
-//	@RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
-//	public String loginForm(Locale locale, Model model, @PathVariable String id) {
-//		logger.info("Welcome home! The client locale is {}.", locale);
-//		
-//		Date date = new Date();
-//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-//		
-//		String formattedDate = dateFormat.format(date);
-//		
-//		model.addAttribute("serverTime", formattedDate );
-//		
-//		return "security/loginForm";
-//	}
 
 }
